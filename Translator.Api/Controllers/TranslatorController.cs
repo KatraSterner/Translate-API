8c7e47a1775bc977e29356953e2acf7a3023aa05
KatraSterner <Rollie406@gmail.com>
2026-03-17 11:04:39 -0500

    adding some frontend files -katra

---
+++ Translator.Api/Controllers/TranslatorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Translator.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TranslatorController() : ControllerBase
+{
+    
+}
